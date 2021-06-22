a6fb41547e55d8d3dc7be255a3f9f40368520b24
segunoz <segunoz1@gmail.com>
2021-06-22 09:33:00 +0100

    Update assignment.ts

--- assignment.ts
+++ assignment.ts
@@ -5,19 +5,32 @@ class car{
     isEngineFaulty: Boolean;
 
     constructor(engineType: String, chassisType: String, isEngineRunning: Boolean,  isEineFaulty: Boolean){
-        this.engineType = engineType
-        this.chassisType = chassisType
-        this.isEngineRunning =  isEngineRunning
-        this.isEngineFaulty = isEngineRunning
+        this.engineType = engineType;
+        this.chassisType = chassisType;
+        this.isEngineRunning =  isEngineRunning;
+        this.isEngineFaulty = isEngineRunning;
 
     }
 
 
 
     displayCarDetails(): void{
-        let carDetails: String = "This car has a ${this.engineType} engine and a ${this.chassis}"
-        console.log(carDetails)
+        let carDetails: String = "This car has a ${this.engineType} engine and a ${this.chassis}";
+        console.log(carDetails);
+    }
+    
+    checkEngineRunning(): Boolean{
+         console.log(isEngineRunning);  
+    }
+    
+    checkEngineFaulty(): Boolean{
+        console.log(isEngineFaulty);
     }
     
 
 }
+
+let toyota = new car("v6", "outmoded", true, false);
+toyota.displayCarDetails();
+toyota.checkEngineRunning;
+toyota.checkEngineFaulty;
